2425055305a580a85a10ecd2e64b2635f0b9199e
Evdokimov Denis <analitik9@gmail.com>
2022-10-25 13:47:29 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Denis Evdokimov on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
